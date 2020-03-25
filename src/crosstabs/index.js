3ae0d2bbe8ea1bea4062e2164adbf84550d05501
boutique-lafolie <mehdiayad@hotmail.fr>
2020-03-25 21:28:10 +0000

    updates listener

--- src/crosstabs/index.js
+++ src/crosstabs/index.js
@@ -3,10 +3,12 @@ import userStore from '../store/userStore'
 var listener = window.addEventListener('storage', function(event){
     if (event.key == 'event-logout') {
         console.log('logout event')
-        userStore.commit('logout') //TODO FIX INFINITE VUEX DATA UPDATING
+        // COMMENT LINE BELOW FOR STABLE VERSION
+        userStore.commit('logout') //TODO FIX INFINITE VUEX DATA UPDATING 
     }
     if (event.key == 'event-login') { 
         console.log('login event')
+        // COMMENT LINE BELOW FOR STABLE VERSION
         userStore.commit('login') //TODO FIX INFINITE VUEX DATA UPDATING
     }
 });
